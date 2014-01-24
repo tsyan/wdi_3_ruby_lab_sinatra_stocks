ddcfcb1b3cd55f55f50f501b0432d16f9c3dc7ae
tsyan <anna.tsykalova@gmail.com>
2014-01-24 14:12:51 -0500

    here are some stocks, yo

--- main.rb
+++ main.rb
@@ -1,8 +1,11 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'yahoofinance'
+require 'yahoo_finance'
 
-get '/' do
+get '/:symbol' do |symbol|
+	@price_array = YahooFinance.quotes([symbol.to_sym], [:last_trade_price])
+  @price = @price_array[0][:last_trade_price]
+ 	@name = symbol
   erb :quote
 end
